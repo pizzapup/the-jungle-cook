8ba6dd0da559b4936a03e6d68d495fe8e51454b2
pizzapup <57334884+pizzapup@users.noreply.github.com>
2021-12-12 15:38:25 -0500

    update

--- public/app/app.js
+++ public/app/app.js
@@ -1,70 +1,49 @@
 /* ------------------------------------------------------ */
-/*                     HAMBURGER MENU                     */
+/*                       HASHCHANGE                       */
 /* ------------------------------------------------------ */
-// hamburger menu for screens under 960px
-function hamburger() {
-  $(".bars").click(function (e) {
-    $(".bars").toggleClass("active");
-    $(".links").toggleClass("active");
-  });
-  $(".links a").click(function (e) {
-    $(".bars").toggleClass("active");
-    $(".links").toggleClass("active");
-  });
+//
+/* --------- changeRoute() based on hash change: -------- */
+function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  let currentHero = $("#heroWrapper").attr("class");
+  if (pageID != "") {
+    $("#heroWrapper").attr("class", pageID);
+    if (pageID != "view-recipe") {
+      $.get(`${pageID}.html`, function (data) {
+        $("#pageContentApp").html(data);
+        if ((pageID = "browse")) {
+          $(".loadRButtn").click();
+        }
+        if (_userProfileInfo.firstName) {
+          $(".loadRButtn").prop("disabled", true);
+          $(".createRecipeSubmit").prop("disabled", true);
+
+          $(".user-greeting").html(
+            `${_userProfileInfo.firstName}` +
+              ` ` +
+              `${_userProfileInfo.lastName}`
+          );
+          //
+        }
+        if ((pageID = "your-recipe")) {
+          $(".loadRButtn").click();
+        }
+      });
+    }
+  } else {
+    $.get(`home.html`, function (data) {
+      $("#pageContentApp").html(data);
+    });
+  }
+}
+function initURLListener() {
+  window.onhashchange = changeRoute;
+  changeRoute();
 }
 
-/* -------------- menu nav logo and footer -------------- */
-/* ------------------- footer content ------------------- */
-$("#footer-content").html(`
-<div class="footer-content">
-<div class="copyright-footer">Copyright © 2019 The Jungle Cook</div>
-<div class="footer-links">
-  <a href="#login">Login</a>
-  <a href="#browse">Recipes by Category</a>
-  <a href="">Privacy and Copyright</a>
-  <a href="#create-recipe" >Create Recipe</a>
-  <a href="#view-recipe">Your Recipes</a>
-</div>
-  <div class="social facebook">
-    <a href=""
-      ><img src="images/facebook.svg" alt="Facebook.com"
-    /></a>
-  </div>
-  <div class="social facebook">
-    <a href=""
-      ><img src="images/instagram.svg" alt="instagram.com"
-    /></a>
-  </div>
-</div>`);
+/* ------------------------------------------------------ */
 
-/* --------------------- navigation --------------------- */
-$(".navigation-bar").html(`
-<!-- ---------------- logo ----------------- -->
-<a href="index.html">
-<div id="logo-title" class="page-title-norm">
-  <span class="circle blue"></span>
-  <span class="circle yellow"></span>
-  <span class="circle green"></span>
-  <span class="circle pink"></span>
-  <h1>The Jungle Cook</h1>
-</div>
-</a>
-<div class="nav-holder">
-<div class="bars">
-  <span class="bar"></span>
-  <span class="bar"></span>
-  <span class="bar"></span>
-</div>
-<div class="links" >
-<a href="#home">Home</a>
-<a href="#browse">Browse</a>
-<a href="#create-recipe">Create Recipe</a>
-<a href="#your-recipe">Your Recipes</a>
-<span id="change-logBtn" ><a href="#login" class="login-button">Login</a></span></div>
-</div>
-`);
-/* ------------------------------------------------------ */
-/* ------------------------------------------------------ */
 $(document).ready(function () {
-  hamburger();
+  initURLListener();
 });
--- public/app/model.js
+++ public/app/model.js
@@ -174,6 +174,69 @@ var RECIPES = [
 ];
 var currentList = "";
 /* ------------------------------------------------------ */
+
+function hamburger() {
+  $(".bars").click(function (e) {
+    $(".bars").toggleClass("active");
+    $(".links").toggleClass("active");
+  });
+  $(".links a").click(function (e) {
+    $(".bars").toggleClass("active");
+    $(".links").toggleClass("active");
+  });
+}
+/* ------------------- footer content ------------------- */
+$("#footer-content").html(`
+<div class="footer-content">
+<div class="copyright-footer">Copyright © 2019 The Jungle Cook</div>
+<div class="footer-links">
+  <a href="#login">Login</a>
+  <a href="#browse">Recipes by Category</a>
+  <a href="">Privacy and Copyright</a>
+  <a href="#create-recipe" >Create Recipe</a>
+  <a href="#view-recipe">Your Recipes</a>
+</div>
+  <div class="social facebook">
+    <a href=""
+      ><img src="images/facebook.svg" alt="Facebook.com"
+    /></a>
+  </div>
+  <div class="social facebook">
+    <a href=""
+      ><img src="images/instagram.svg" alt="instagram.com"
+    /></a>
+  </div>
+</div>`);
+
+/* --------------------- navigation --------------------- */
+$(".navigation-bar").html(`
+<!-- ---------------- logo ----------------- -->
+<a href="index.html">
+<div id="logo-title" class="page-title-norm">
+  <span class="circle blue"></span>
+  <span class="circle yellow"></span>
+  <span class="circle green"></span>
+  <span class="circle pink"></span>
+  <h1>The Jungle Cook</h1>
+</div>
+</a>
+<div class="nav-holder">
+<div class="bars">
+  <span class="bar"></span>
+  <span class="bar"></span>
+  <span class="bar"></span>
+</div>
+<div class="links" >
+<a href="#home">Home</a>
+<a href="#browse">Browse</a>
+<a href="#create-recipe">Create Recipe</a>
+<a href="#your-recipe">Your Recipes</a>
+<span id="change-logBtn" ><a href="#login" class="login-button">Login</a></span></div>
+</div>
+`);
+$(document).ready(function () {
+  hamburger();
+});
 function loadBrowseRecipes() {
   // var currentList = RECIPES;
   loadRecipes(RECIPES);
@@ -751,6 +814,7 @@ function initFirebase() {
     }
   });
 }
+
 // uid = initFirebase;
 $(document).ready(function () {
   try {
